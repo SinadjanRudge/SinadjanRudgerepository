94deb8b3f78b2f51a5efd3964f216d0280c9429d
SinadjanRudge <rudgesinadjan@gmail.com>
2023-03-04 16:13:13 +0800

    04/03/2023 ListView

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,35 +2,37 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity{
+    ListView listView;
+    TextView textView;
+    String[] listItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list);
 
+        listView=(ListView)findViewById(R.id.listView);
+        listItem=getResources().getStringArray(R.array.Robots);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.my_list,R.id.textView,listItem);
+        listView.setAdapter(adapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+                                        {
+                                            @Override
+                                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                                String value=adapter.getItem(position);
+                                                Toast.makeText(getApplicationContext(), value,Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
 
-        Log.v(TAG, "This is a verbose log");
-        Log.d(TAG, "This is a debug log");
-        Log.i(TAG, "This is an info log");
-        Log.w(TAG, "This is a warn log");
-        Log.e(TAG, "This is an error log");
-
-        /*Button button = (Button) findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "Button Clicked");
-                Intent intent = new Intent( MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        }); */
+        );
     }
 }
