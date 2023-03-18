f4f5e60dc7779368d317b917fde1ae54b99b9482
SinadjanRudge <rudgesinadjan@gmail.com>
2023-03-18 20:54:26 +0800

    18/03/2023 UI components

---
+++ app/src/main/java/com/example/myapplication/DialogFragment.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+public class DialogFragment extends androidx.fragment.app.DialogFragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        return inflater.inflate(R.layout.dialog_fragment,container, false);
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,36 +1,70 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("GfG | ActionBar");
+        actionBar.setSubtitle("Design a custom Action Bar");
+        actionBar.setIcon(R.mipmap.ic_launcher);
 
-
-        Log.v(TAG, "This is a verbose log");
-        Log.d(TAG, "This is a debug log");
-        Log.i(TAG, "This is an info log");
-        Log.w(TAG, "This is a warn log");
-        Log.e(TAG, "This is an error log");
-
-        /*Button button = (Button) findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "Button Clicked");
-                Intent intent = new Intent( MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        }); */
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
     }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected (@NonNull MenuItem item ){
+
+        switch (item.getItemId()) {
+            case R.id.search:
+                showAlertDialog();
+                break;
+            case R.id.refresh:
+                DialogFragment dialogFragment = new DialogFragment();
+                dialogFragment.show(getSupportFragmentManager(), "My Fragment");
+                break;
+            case R.id.copy:
+                Toast.makeText(this, "Copy Clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void showAlertDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert!")
+                .setMessage("Danger you are failing");
+        builder.setPositiveButton("I know!", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int id){
+
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
